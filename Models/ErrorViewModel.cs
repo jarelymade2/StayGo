9aa328aa2c4418ce10e2da8044643e8d9968e631
StayGo-Desktop <jazarelly_zapata@usmp.pe>
2025-08-28 04:39:15 -0500

    inicio, iniciar sesion y registrar

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AVANCE_DESKTOP_FIGMA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
