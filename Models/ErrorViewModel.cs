d3278a3a89927f8ccad56dd072a5487d9251ea89
Jazarelly Zapata <147675418+M4R4CUY4@users.noreply.github.com>
2025-08-28 04:52:32 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AVANCE_DESKTOP_FIGMA.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
